f2cffcc76e9b5fb7515f4bcfd3a95021ec35a507
nadams <nadams@node-3.net>
2015-03-01 11:01:32 -0700

    Removing garbage from script

--- compress-maps.sh
+++ compress-maps.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-maps=$(ls maps/*.wad)
-zdbsp="zdbsp"
-
-if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-  $zdbsp = "zdbsp.exe"
-fi
-
-if [ -n "$(hash $zdbsp)" ]; then
-  echo "Could not find $zdbsp in \$PATH"
-  exit 1
-fi
-
-for map in $maps
+for map in $(ls maps/*.wad)
 do
-	$zdbsp \
+	zdbsp \
 		--empty-blockmap \
 		--zero-reject \
 		--extended \
